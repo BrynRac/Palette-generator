24c089311f466edee0af04a1dafcf0eab7da9081
Bryan <brynrace@gmail.com>
2020-02-02 21:26:13 -0800

    complete

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//static folder
-app.use(express.static(path.join(__dirname, "public")));
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.render("pages/index", { title: "Grid Schemer" });
 });
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log(`Starting port ${PORT}...`));
